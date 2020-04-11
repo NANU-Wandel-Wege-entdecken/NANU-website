b0cc514107a6f4a5b8aac36b520b06e926eb13ad
ibes <mail@sebastian-gaertner.com>
2020-04-11 23:58:05 +0200

    Extend setup.sh with .env notice

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,4 +6,8 @@ git config core.hooksPath git-hooks
 # run installers and things
 ./bin/compile.sh
 
+if [ ! -f "./.env" ];
+then echo ".env missing - please copy .env.example and adjust to your needs";
+fi
+
 echo 'setup complete'
