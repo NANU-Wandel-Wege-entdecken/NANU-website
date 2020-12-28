dc6f5206d0dce3ec4d66ce8d5d7382cbd9e41be1
Michael Voit <michael@digitalfellows.eu>
2020-12-28 22:34:23 +0100

    [frontend] style articles section on front-page

--- public/themes/nanu/front-page.php
+++ public/themes/nanu/front-page.php
@@ -11,40 +11,66 @@
 
 get_header(); ?>
 
-<div id="hero" class="bg-beige py-20">
+<section id="hero" class="bg-beige py-20">
     <div class="container m-auto px-2">
         HERO
     </div>
-</div>
+</section>
 
 
-<div id="articles" class="container m-auto px-2 py-20">
+<section id="articles" class="container m-auto px-2 sm:px-5 py-20">
     <h2><span class="text-red">Aktuell</span> beliebte Artikel</h2>
-</div>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main grid grid-cols-1 md:grid-cols-2 gap-10" role="main">
+
+        <?php // WP_Query arguments
+        $args = array(
+            'posts_per_page'         => 4,
+            'order' => 'DESC',
+            'orderby' => 'date',
+        );
+
+        // The Query
+        $query = new WP_Query( $args );
+
+        // The Loop
+        if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                get_template_part( 'views/content-teaser' );
+            }
+        } else {
+            // no posts found
+        }
+        ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+    </section>
 
 
-<div id="cta-newsletter" class="bg-pink border-t-4 border-b-4 border-pink-light text-white py-20">
+<section id="cta-newsletter" class="bg-pink border-t-4 border-b-4 border-pink-light text-white py-20">
     <div class="container m-auto px-2">
         CTA NEWSLETTER
     </div>
-</div>
+</section>
 
 
-<div id="books" class="container m-auto px-2 py-20">
+<section id="books" class="container m-auto px-2 py-20">
     <h2>Unsere <span class="text-red">Buchempfehlungen</span></h2>
-</div>
+</section>
 
 
-<div id="cta-participate" class="bg-grey py-20">
+<section id="cta-participate" class="bg-grey py-20">
     <div class="container m-auto px-2">
         CTA PARTICIPATE
     </div>
-</div>
+</section>
 
 
-<div id="pages" class="container m-auto px-2 py-20">
+<section id="pages" class="container m-auto px-2 py-20">
 PAGES
-</div>
+</section>
 
 <?php
 get_footer();
