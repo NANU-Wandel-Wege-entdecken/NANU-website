2dc3c14fec3139a637d649143cbdf48eb411edfe
Michael Voit <michael@digitalfellows.eu>
2020-12-28 17:50:31 +0100

    [frontend] add structure to footer

--- public/themes/nanu/footer.php
+++ public/themes/nanu/footer.php
@@ -13,13 +13,21 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container-fluid" role="contentinfo">
+	<footer id="colophon" class="site-footer container-fluid text-sm" role="contentinfo">
 
-		<div class="site-info py-8 bg-beige text-center">
-            FOOTER
-        </div><!-- .site-info -->
-        <div class="py-2 bg-pink text-white">
-            <p class="pb-0">- mit Wildheit und Liebe - seit 2009 -</p>
+		<div class="py-8 px-6 grid grid-cols-4 gap-2 bg-beige text-grey-footer">
+            <div>Unsere Empfehlungen</div>
+            <div>Neue Artikel</div>
+            <div>Rechtliches</div>
+            <div>Instagram</div>
+        </div>
+        <div class="py-2 px-4 grid grid-cols-2 gap-2 bg-pink text-white">
+            <div>
+                <p class="pb-0">- mit Wildheit und Liebe - seit 2009 -</p>
+            </div>
+            <div class="text-right">
+                social media icons
+            </div>
         </div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
