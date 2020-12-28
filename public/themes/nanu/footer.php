b4cbf51888b286e0b0640ff0aea8761174636cc1
Michael Voit <michael@digitalfellows.eu>
2020-12-28 22:35:39 +0100

    [frontpage] overwidth container for footer content

--- public/themes/nanu/footer.php
+++ public/themes/nanu/footer.php
@@ -15,18 +15,22 @@
 
 	<footer id="colophon" class="site-footer container-fluid text-sm" role="contentinfo">
 
-		<div class="py-8 px-6 grid grid-cols-4 gap-2 bg-beige text-grey-footer">
-            <div>Unsere Empfehlungen</div>
-            <div>Neue Artikel</div>
-            <div>Rechtliches</div>
-            <div>Instagram</div>
-        </div>
-        <div class="py-2 px-4 grid grid-cols-2 gap-2 bg-pink text-white">
-            <div>
-                <p class="pb-0">- mit Wildheit und Liebe - seit 2009 -</p>
+		<div class="py-8 px-6 bg-beige text-grey-footer">
+            <div class="m-auto grid grid-cols-4 gap-2" style="max-width: 1800px;">
+                <div>Unsere Empfehlungen</div>
+                <div>Neue Artikel</div>
+                <div>Rechtliches</div>
+                <div>Instagram</div>
             </div>
-            <div class="text-right">
-                social media icons
+        </div>
+        <div class="py-2 px-4 bg-pink text-white">
+            <div class="m-auto grid grid-cols-2 gap-2" style="max-width: 1800px;">
+                <div>
+                    <p class="pb-0">- mit Wildheit und Liebe - seit 2009 -</p>
+                </div>
+                <div class="text-right">
+                    social media icons
+                </div>
             </div>
         </div>
 	</footer><!-- #colophon -->
